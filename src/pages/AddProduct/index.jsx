4dc38aa6e5723092d9b06e1822878f6dbe28ea8e
Serhii <sirozhka.v@gmail.com>
2024-04-23 12:43:43 +0200

    admin panel add product functionality

--- src/pages/AddProduct/index.jsx
+++ src/pages/AddProduct/index.jsx
@@ -1,4 +1,7 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { useEffect, useState } from 'react';
+import app from '../../../firebase';
 
 const AddProduct = () => {
   const [name, setName] = useState('');
@@ -7,8 +10,92 @@ const AddProduct = () => {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState('');
   const [img, setImg] = useState(undefined);
+  const [imgURL, setImgURL] = useState('');
+  const [imgPerc, setImgPerc] = useState(0);
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState('');
 
-  console.log(img);
+  const clearImgFields = () => {
+    setImg(undefined);
+    setImgURL('');
+    setImgPerc(0);
+  };
+
+  const clearFields = () => {
+    setName('');
+    setBrandname('');
+    setPrice('');
+    setDescription('');
+    setQuantity('');
+  };
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const categRes = await axios.get('http://localhost:8800/api/categories');
+        setCategories(categRes.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  const uploadFile = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, `images/${fileName}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImgPerc(Math.round(progress));
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgURL(downloadURL);
+        });
+      },
+    );
+  };
+
+  useEffect(() => {
+    img && uploadFile(img);
+  }, [img]);
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        `http://localhost:8800/api/products`,
+        { name, brandname, imgURL, price, quantity, description, category },
+        {
+          withCredentials: true,
+        },
+      );
+
+      clearImgFields();
+      clearFields();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <section>
@@ -20,12 +107,18 @@ const AddProduct = () => {
               <form className="form-list">
                 <div className="form-list__item">
                   <label htmlFor="Middlename">Категорія</label>
-                  <select name="Category" tabIndex={0}>
+                  <select
+                    onChange={(e) => setCategory(e.target.value)}
+                    name="Category"
+                    tabIndex={0}>
                     <option value disabled="disabled">
                       Виберіть категорію
                     </option>
-                    <option value="Категорія 1">Категорія1</option>
-                    <option value="Категорія 1">Категорія2</option>
+                    {categories.map((category) => (
+                      <option key={category._id} value={category.name}>
+                        {category.name}
+                      </option>
+                    ))}
                   </select>
                 </div>
                 <div className="form-list__item">
@@ -83,15 +176,27 @@ const AddProduct = () => {
                   </div>
                 </div>
               </form>
-              <button className="addproduct">Додати товар</button>
+              <button onClick={handleUpload} className="addproduct">
+                Додати товар
+              </button>
             </div>
             <div className="addimage-block">
               <h3>Додати картинку товару</h3>
-              <input
-                className="addimage"
-                onChange={(e) => setImg(e.target.files[0])}
-                type="file"
-                accept="image/*"></input>
+              {imgPerc > 0 && imgPerc != 100
+                ? 'Uploading:' + imgPerc + '%'
+                : !imgURL && (
+                    <input
+                      className="addimage"
+                      onChange={(e) => setImg(e.target.files[0])}
+                      type="file"
+                      accept="image/*"></input>
+                  )}
+              {imgURL ? <img className="img-preview" src={imgURL} alt="category pic" /> : <></>}
+              {imgURL && (
+                <button className="addproduct" onClick={clearImgFields}>
+                  Очистити
+                </button>
+              )}
             </div>
           </div>
         </div>
