57fa55c8b7a596439c942658d92ff6af7ca5f8ba
Serhii <sirozhka.v@gmail.com>
2024-05-10 13:00:58 +0200

    routes changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import AddCategory from './pages/AddCategory';
 import AllOrders from './pages/AllOrders';
 import NoMatch from './pages/NoMatch';
 import UserOrders from './pages/UserOrders';
+import DropshipApplication from './pages/DropshipApplication';
 
 function App() {
   return (
@@ -21,11 +22,14 @@ function App() {
         <Route path="/categories/:categoryname" element={<Category />} />
         <Route path="/categories/:categoryname/:productId" element={<Product />} />
         <Route path="/favourites" element={<Favourites />} />
-        <Route path="/customer-area/" element={<Profile />} />
-        <Route path="/customer-area/all-orders" element={<AllOrders />} />
-        <Route path="/customer-area/orders" element={<UserOrders />} />
-        <Route path="/customer-area/add-product" element={<AddProduct />} />
-        <Route path="/customer-area/add-category" element={<AddCategory />} />
+        <Route path="customer-area">
+          <Route index element={<Profile />} />
+          <Route path="all-orders" element={<AllOrders />} />
+          <Route path="orders" element={<UserOrders />} />
+          <Route path="add-product" element={<AddProduct />} />
+          <Route path="add-category" element={<AddCategory />} />
+          <Route path="dropship-program" element={<DropshipApplication />} />
+        </Route>
         <Route path="*" element={<NoMatch />} />
       </Routes>
       <Footer />
